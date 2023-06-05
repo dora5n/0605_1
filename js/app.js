005d3502dfca7bf3363ff7194a09881120d31cb9
dora5n <Moe4dora5n@gmail.com>
2023-06-05 16:18:44 +0900

    多次元配列の練習

--- js/app.js
+++ js/app.js
@@ -4,11 +4,19 @@ console.log("まえさこ" + 80 * rate + "点");
 console.log("みつ君" + 90 * rate + "点");
 console.log("かほ" + 70 * rate + "点");
 
+
 let animals = ["dog","cat","lion"];
 console.log(animals[1]);
 animals[1] = "tiger";
 console.log(animals[1]);
 
+
+let data = [
+  ["太郎","24","180cm","65kg"],
+  ["次郎","21","176cm","63kg"],
+  ["三郎","19","198cm","89kg"]
+];
+console.log(data[1][2]);
 
 
 
