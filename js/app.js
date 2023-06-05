845e50d9a0a754241dc069616150d17f127a3177
dora5n <Moe4dora5n@gmail.com>
2023-06-05 16:34:07 +0900

    名前

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // alert(1111);
 let rate = 0.8;
-console.log("まえさこ" + 80 * rate + "点");
-console.log("みつ君" + 90 * rate + "点");
-console.log("かほ" + 70 * rate + "点");
+console.log("太郎" + 80 * rate + "点");
+console.log("次郎" + 90 * rate + "点");
+console.log("三郎" + 70 * rate + "点");
 
 
 let animals = ["dog","cat","lion"];
